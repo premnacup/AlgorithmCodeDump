13082074abff0ca8066503cc955e346fbbd417f9
premnacup <premprangja@gmail.com>
2025-03-23 16:13:46 +0700

    Update HW6_1.cpp

--- HW6/HW6_1.cpp
+++ HW6/HW6_1.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Prints the array x from index 0 to n-1.
- */
 void printAns(char x[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -13,50 +10,41 @@ void printAns(char x[], int n)
     cout << endl;
 }
 
-/**
- * Rotates the array x from index i to j to the right by one position.
- */
 void rightRotate(char x[], int i, int j)
 {
-    int temp = x[j];            // Store the last element of the rotation
-    for (int k = j; k > i; k--) // Move all elements from j to i one position to the right
+    int temp = x[j];
+    for (int k = j; k > i; k--)
     {
         x[k] = x[k - 1];
     }
-    x[i] = temp; // Put the stored element in the position of the first element of the rotation
+    x[i] = temp;
 }
 
-/**
- * Rotates the array x from index i to j to the left by one position.
- */
 void leftRotate(char x[], int i, int j)
 {
-    int temp = x[i];            // Store the first element of the rotation
-    for (int k = i; k < j; k++) // Move all elements from i to j one position to the left
+    int temp = x[i];
+    for (int k = i; k < j; k++)
     {
         x[k] = x[k + 1];
     }
-    x[j] = temp; // Put the stored element in the position of the last element of the rotation
+    x[j] = temp;
 }
 
-/**
- * Generates all permutations of the array x from index start to end.
- */
 void permute(char x[], int start, int end)
 {
-    if (start == end) // If we have reached the end of the array, print the permutation
+    if (start == end)
     {
         printAns(x, end);
         return;
     }
 
-    permute(x, start + 1, end); // Recursively generate permutations for the rest of the array
+    permute(x, start + 1, end);
 
-    for (int j = start + 1; j < end; j++) // For each element in the array from start+1 to end-1
+    for (int j = start + 1; j < end; j++)
     {
-        rightRotate(x, start, j);   // Rotate the array to the right by one position
-        permute(x, start + 1, end); // Recursively generate permutations for the rest of the array
-        leftRotate(x, start, j);    // Rotate the array to the left by one position
+        rightRotate(x, start, j);
+        permute(x, start + 1, end);
+        leftRotate(x, start, j);
     }
 }
 
